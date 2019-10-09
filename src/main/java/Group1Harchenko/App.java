102b7876e333a110d27fae7ea4d23a314a68a8fa
TvoiFace <bogdan.xarchenko@gmail.com>
2019-10-09 21:36:51 +0300

    Commit number 2

--- src/main/java/Group1Harchenko/App.java
+++ src/main/java/Group1Harchenko/App.java
@@ -4,7 +4,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        int arr[] = {1,2,3,4,5,6,7,8,9};
+        int arr[] = {1,2,3,4,5,6,7,8,9,10};
         System.out.println( "Ur array: " );
         for(int i=0;i<arr.length;i++)
         {
