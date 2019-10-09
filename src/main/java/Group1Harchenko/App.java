28d2a7248ab57a2398b35413a46b35bd3ab2be3a
TvoiFace <bogdan.xarchenko@gmail.com>
2019-10-09 21:34:22 +0300

    Commit number 1

---
+++ src/main/java/Group1Harchenko/App.java
@@ -0,0 +1,14 @@
+package Group1Harchenko;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        int arr[] = {1,2,3,4,5,6,7,8,9};
+        System.out.println( "Ur array: " );
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println( arr[i] );
+        }
+    }
+}
